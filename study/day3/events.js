0a1aa093f15e3d8022f182e282a37928d507042e
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2023-09-06 17:44:57 +0900

    event 종류
    - window 객체 event
    - 이벤트 할당

--- study/day3/events.js
+++ study/day3/events.js
@@ -1,33 +1,64 @@
-const title = document.querySelector('div.hello:first-child h1');
+const h1 = document.querySelector('div.hello:first-child h1');
 
-console.log(title);
+console.log(h1);
 // console.log : object로 표시한 element를 보여줌
 // on~ : 이벤트
 
-title.innerText = 'Click ME!';
+h1.innerText = 'Click ME!';
 
-// title.style.color = 'blue';
+// h1.style.color = 'blue';
 // style도 javascript에서 변경할 수 있음.
 
-function handleTitleClick() {
-  title.style.color = 'tomato';
-  console.log('title was clicked!');
+function handleH1Click() {
+  h1.style.color = 'tomato';
+  console.log('h1 was clicked!');
 }
 
 function handleMouseEnter() {
   console.log('Mouse is here!');
-  title.innerText = 'Mouse is here!';
+  h1.innerText = 'Mouse is here!';
 }
 function handleMouseLeave() {
-  title.innerText = 'Mouse is gone!';
+  h1.innerText = 'Mouse is gone!';
 }
 
-title.addEventListener('click', handleTitleClick);
-title.addEventListener('mouseenter', handleMouseEnter);
-title.addEventListener('mouseleave', handleMouseLeave);
+h1.addEventListener('click', handleH1Click);
+// h1.onclick = handleH1Click;
+// 함수를 할당하는 방식으로도 사용할 수 있다.
+h1.addEventListener('mouseenter', handleMouseEnter);
+h1.addEventListener('mouseleave', handleMouseLeave);
 // event를 listen 하는 것. 어떤 event를 listen하고싶은지 적어야 함
 // 해당 event에 어떤 function을 동작시킬지 argument를 2개 넘겨줘야 함
 // function name만 작성하고 함수 실행을 시켜선 안됨.
 
-console.dir(title);
+// 객체.removeEventListener 이벤트를 지울수도 있음
+// 따라서 함수를 할당하는 방식보다 더 나음
+
+console.dir(h1);
 // element에 가능한 event를 javascript 관점에서 디테일하게 보고싶을 때
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = 'pink';
+  // html의 body, head, title과 같은 메인 태그들은 바로 불러올 수있음
+  // 나머지 element들은 querySeletor or getElementByid 등으로 불러와야함
+}
+
+function handleWindowCopy() {
+  alert('copy!');
+}
+
+function handleWindowOffline() {
+  alert('SOS no WIFI');
+}
+
+function handleWindowOnline() {
+  alert('ALL GOOD!');
+}
+
+// window 객체의 이벤트를 사용할 수도 있음
+// resize = 화면 사이즈 조정
+// online & offline = wifi 연결
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy', handleWindowCopy);
+window.addEventListener('offline', handleWindowOffline);
+window.addEventListener('online', handleWindowOnline);
