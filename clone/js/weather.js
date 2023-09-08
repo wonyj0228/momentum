aa956f2dcd7c92cd0c019f33cfe7ca7c2af9c395
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2023-09-08 23:39:25 +0900

    weather 정보
    - fetch
    - .then()
    - .json()
    - navigator.geolocation.getCurrentPosition

---
+++ clone/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = config.apikey;
+
+function onGeoOk(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
